648d43dee2fe5db9e0518eb6eface71072357d9f
emattia <emattia4495@gmail.com>
2024-12-09 20:46:29 +0000

    add comments

--- torchrun/01-all-reduce-profile/flow.py
+++ torchrun/01-all-reduce-profile/flow.py
@@ -1,4 +1,5 @@
 from metaflow import FlowSpec, step, torchrun, kubernetes, current
+from metaflow.profilers import gpu_profile
 
 
 # REPLACE WITH YOUR DESIRED IMAGE AND RESOURCE SETTINGS 
@@ -9,10 +10,20 @@ N_GPU = 1
 
 class TorchrunAllreduce(FlowSpec):
 
+    '''
+    The benchmark performs the following:
+        - Initializes a distributed process group using the specified backend (default: NCCL)
+        - Creates a large tensor (1000³ elements, ~4GB with float32) on each GPU
+        - Verifies correctness of all-reduce operation
+        - Measures performance over multiple trials
+        - Reports throughput and bus bandwidth metrics
+    '''
+
     @step
     def start(self):
         self.next(self.torch_multinode, num_parallel=N_NODES)
 
+    @gpu_profile(interval=1)
     @kubernetes(image=IMAGE, gpu=N_GPU)
     @torchrun
     @step
--- torchrun/02-nccl-all-comms/flow.py
+++ torchrun/02-nccl-all-comms/flow.py
@@ -1,4 +1,5 @@
 from metaflow import FlowSpec, step, torchrun, kubernetes, current
+from metaflow.profilers import gpu_profile
 
 
 # REPLACE WITH YOUR DESIRED IMAGE AND RESOURCE SETTINGS 
@@ -9,10 +10,15 @@ N_GPU = 1
 
 class TorchrunNCCLComms(FlowSpec):
 
+    '''
+    The benchmark performs each of the core NCCL operations.
+    '''
+
     @step
     def start(self):
         self.next(self.torch_multinode, num_parallel=N_NODES)
 
+    @gpu_profile(interval=1)
     @kubernetes(image=IMAGE, gpu=N_GPU)
     @torchrun
     @step
